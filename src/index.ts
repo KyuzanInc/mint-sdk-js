d315baae71b3203c4a04b6c00c2dc9561667112d
tomonari-t <tomo1208.japan@gmail.com>
2021-03-04 07:59:54 +0900

    chore: Fix package.json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { hoo } from './Hoo'
 class AnnapurnaSDK {
   private constructor() {
-    console.log('aaaa')
-  }
-
-  public initialize() {
-    console.log(hoo)
-
-    return new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log('test')
   }
 }
 
