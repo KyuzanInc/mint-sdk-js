31eeba9154590991d4b87c3baf2d2ca43141b9b5
tomonari-t <tomo1208.japan@gmail.com>
2021-03-30 10:36:13 +0900

    feat(example): Add emotion

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,12 +1,33 @@
 import { AppComponent } from 'next/dist/next-server/lib/router/router'
 import { Provider } from 'react-redux'
+import { Global, css } from '@emotion/react'
+import emotionReset from 'emotion-reset'
 import { getStore } from '../redux/getStore'
 import * as React from 'react'
 import Layout from '../components/Layout'
+import { color } from '../style/idnex'
 
 const MyApp: AppComponent = ({ Component, pageProps }) => {
   return (
     <Provider store={getStore()}>
+      <Global
+        styles={css`
+          ${emotionReset}
+          *, html, body {
+            font-family: 'Montserrat', sans-serif;
+            color: ${color.content.dark};
+          }
+
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            font-smoothing: antialiased;
+          }
+        `}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
