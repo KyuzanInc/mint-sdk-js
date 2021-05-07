fc8fa85e53c85dbde5b8793cf1860ecde7b02785
tomonari-t <tomo1208.japan@gmail.com>
2021-05-07 19:07:05 +0900

    fix(example): Fix token

--- example/src/sdk/index.ts
+++ example/src/sdk/index.ts
@@ -6,19 +6,11 @@ export const getSdk = () => {
   if (typeof window === 'undefined') return null
 
   if (typeof sdk === 'undefined') {
-    sdk = new MintSDK(
-      '4bdaee8f-8e23-4913-9858-7a10dd7be877',
-      [4],
-      {
-        fortmatic: {
-          key: DEMO_FORTMATIC_KEY,
-        },
+    sdk = new MintSDK(DEMO_ACCESS_KEY, [4], {
+      fortmatic: {
+        key: DEMO_FORTMATIC_KEY,
       },
-      {
-        backendUrl:
-          'https://asia-northeast1-annapurna-development.cloudfunctions.net',
-      }
-    )
+    })
   }
 
   return sdk
