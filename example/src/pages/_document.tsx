41c7e594f3e48cbd1d2d5b13c0754f3a82d10f02
ReonaN <reona.nagafuchi@gmail.com>
2021-05-08 11:12:52 +0900

    add favicon

--- example/src/pages/_document.tsx
+++ example/src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/images/favicon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
