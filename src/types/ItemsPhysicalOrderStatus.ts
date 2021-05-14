8f6608e62fd642cea994a5a40efcd7d50913df99
tomonari-t <tomo1208.japan@gmail.com>
2021-05-14 09:17:11 +0900

    fix: Fix type

--- src/types/ItemsPhysicalOrderStatus.ts
+++ src/types/ItemsPhysicalOrderStatus.ts
@@ -2,7 +2,7 @@
  *
  */
 export const itemsPhysicalOrderStatusList = [
-  'addressIsBlank', // エンドユーザーからの住所登録待ち
+  'shippingInfoIsBlank', // エンドユーザーからの住所登録待ち
   'wip', // Mint管理者側の配送アクション待ち
   'shipped', // 出荷済み
 ] as const
