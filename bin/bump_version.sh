7bfc5e54bc4ced70ebf455503639d3edf92a36f6
玲緒菜 永渕 <reona.nagafuchi@gmail.com>
2021-05-25 01:21:32 +0900

    add workflows

---
+++ bin/bump_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOCAL_VERSION=`node -e "console.log(require('./package.json').version)"`
+REMOTE_VERSION=`npm view "" dist-tags.latest || echo`
+if ["$LOCAL_VERSION" = "$REMOTE_VERSION"]
+then
+  npm version patch -m "Bump version to %s by Github Action"
+
+else
+  git tag -m "Bump version to $LOCAL_VERSION by Github Action" "v$LOCAL_VERSION"
+fi
+git push --tags main
