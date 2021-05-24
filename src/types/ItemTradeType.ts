407bafc53906868dff749a81ce55126f2b484452
tomonari-t <tomo1208.japan@gmail.com>
2021-05-24 15:01:31 +0900

    style: Do format

--- src/types/ItemTradeType.ts
+++ src/types/ItemTradeType.ts
@@ -1,2 +1,6 @@
-export const itemsTradeTypes = ['fixedPrice', 'auction', 'autoExtensionAuction'] as const
+export const itemsTradeTypes = [
+  'fixedPrice',
+  'auction',
+  'autoExtensionAuction',
+] as const
 export type ItemTradeType = typeof itemsTradeTypes[number]
