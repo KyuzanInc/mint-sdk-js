17c6264dc353f59bc13edc1c993cbf6eb23246af
tomonari-t <tomo1208.japan@gmail.com>
2021-06-10 18:15:21 +0900

    major (example): Test用のKeyを戻した

--- example/src/sdk/index.ts
+++ example/src/sdk/index.ts
@@ -6,19 +6,11 @@ export const getSdk = () => {
   if (typeof window === 'undefined') return null
 
   if (typeof sdk === 'undefined') {
-    sdk = new MintSDK(
-      '4bdaee8f-8e23-4913-9858-7a10dd7be877',
-      [4, 80001],
-      {
-        fortmatic: {
-          key: DEMO_FORTMATIC_KEY,
-        },
+    sdk = new MintSDK(DEMO_ACCESS_KEY, [4, 80001], {
+      fortmatic: {
+        key: DEMO_FORTMATIC_KEY,
       },
-      {
-        backendUrl:
-          'http://localhost:5500/annapurna-development/asia-northeast1',
-      }
-    )
+    })
   }
 
   return sdk
