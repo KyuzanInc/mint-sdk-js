1331ca5ac468f02ee0210197e48a5169a9582603
tomonari-t <tomo1208.japan@gmail.com>
2021-06-22 16:38:04 +0900

    (example): Dialogコンポーネントの追加

---
+++ example/src/components/molecules/Dialog/index.stories.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Dialog } from '.'
+
+export const Basic: React.VFC = () => {
+  const [open, setOpen] = useState(true)
+  return (
+    <Dialog
+      isOpen={open}
+      handleConfirm={() => setOpen(false)}
+      title={'ネットワークを変更してください'}
+      content={
+        'オークションに入札するためにはRinkebyネットワークに変更してください'
+      }
+    />
+  )
+}
+
+export const ConfirmText: React.VFC = () => {
+  const [open, setOpen] = useState(true)
+  return (
+    <Dialog
+      isOpen={open}
+      handleConfirm={() => setOpen(false)}
+      confirmText={'接続する'}
+      title={'ネットワークを変更してください'}
+      content={
+        'オークションに入札するためにはRinkebyネットワークに変更してください'
+      }
+    />
+  )
+}
+
+export default {
+  title: 'molecules/Dialog',
+}
