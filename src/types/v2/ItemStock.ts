1fe56c31e44c058a356feb2ba3eceb8e8a370ddf
novi.steven <novisteven97@gmail.com>
2022-07-19 17:35:44 +0700

    update type

--- src/types/v2/ItemStock.ts
+++ src/types/v2/ItemStock.ts
@@ -1,8 +1,21 @@
-import { ItemStock as APIItemStock } from '../../apiClient/api'
+import {
+  ItemStock as APIItemStock,
+  ItemStocksDataItemStocksInner,
+  ItemStockPhysicalShippingInfoStatus,
+} from '../../apiClient/api'
 import { Item } from './Item'
 import { ItemStockStatus } from './ItemStockStatus'
 
 export type ItemStock = Omit<APIItemStock, 'status' | 'item'> & {
   status: ItemStockStatus
   item: Item
+}
+
+export type ItemStockWithPhysicalShippingInfoStatus = Omit<
+  ItemStocksDataItemStocksInner,
+  'status' | 'item' | 'physicalShippingInfoStatus'
+> & {
+  status: ItemStockStatus
+  item: Item
+  physicalShippingInfoStatus: ItemStockPhysicalShippingInfoStatus | null
 }
