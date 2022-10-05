3e7bfb8abc191b0ca5826c91e00ea14fd968a16b
novi.steven <novisteven97@gmail.com>
2022-10-05 17:49:38 +0700

    update networkId

--- src/types/NetworkId.ts
+++ src/types/NetworkId.ts
@@ -1,7 +1,8 @@
-export type NetworkId = 1 | 4 | 80001 | 137 | 31337
+export type NetworkId = 1 | 4 | 5 | 80001 | 137 | 31337
 export const networkIdMapLabel: Record<NetworkId, string> = {
   1: 'main',
   4: 'rinkeby',
+  5: 'goerli',
   137: 'maticMain',
   80001: 'maticMumbai',
   31337: 'hardhat for test',
