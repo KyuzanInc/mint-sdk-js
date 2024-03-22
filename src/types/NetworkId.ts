b32196e3fd616443ad2af14bbdc1dbb4efd11b26
Nguyen Quang <quang@kyuzan.com>
2024-03-22 08:59:12 +0700

    New network

--- src/types/NetworkId.ts
+++ src/types/NetworkId.ts
@@ -1,4 +1,4 @@
-export type NetworkId = 1 | 4 | 5 | 80001 | 137 | 31337 | 11155111 | 1101 | 1442
+export type NetworkId = 1 | 4 | 5 | 80001 | 80002 | 137 | 31337 | 11155111 | 1101 | 1442
 export const networkIdMapLabel: Record<NetworkId, string> = {
   1: 'main',
   4: 'rinkeby',
@@ -7,6 +7,7 @@ export const networkIdMapLabel: Record<NetworkId, string> = {
   1101: 'Polygon zkEVM',
   1442: 'Polygon zkEVM (testnet)',
   80001: 'maticMumbai',
+  80002: 'maticAmoy',
   31337: 'hardhat for test',
   11155111: 'sepolia',
 }
